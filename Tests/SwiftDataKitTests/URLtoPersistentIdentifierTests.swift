3a75b7933e8bbfd38047fc69ea0613aa077d6981
fatbobman <xuyang@me.com>
2023-09-05 19:04:22 +0800

    create URL to PersistentIdentifier tests

--- Tests/SwiftDataKitTests/URLtoPersistentIdentifierTests.swift
+++ Tests/SwiftDataKitTests/URLtoPersistentIdentifierTests.swift
@@ -1,8 +1,34 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Yang Xu on 2023/9/5.
 //
 
 import Foundation
+import SwiftData
+@testable import SwiftDataKit
+import XCTest
+
+@MainActor
+class URLtoPersistentIdentifierTests: XCTestCase {
+    func testURLtoPersistentIdentifier() async throws {
+        let container = try ModelContainer(for: Student.self, configurations: .init(isStoredInMemoryOnly: true))
+        let hander = StudentHandler(modelContainer: container)
+        let student = try await hander.createNewStudent()
+        guard let identifier = student.persistentModelID.uriRepresentation.persistentIdentifier else {
+            XCTFail("Failed to get persistent identifier")
+            return
+        }
+        XCTAssertEqual(student.persistentModelID, identifier)
+    }
+}
+
+extension StudentHandler {
+    func createNewStudent(name: String = "fat", birthOfYear: Int = 2000) throws -> Student {
+        let student = Student(name: name, birthOfYear: birthOfYear)
+        modelContext.insert(student)
+        try! modelContext.save()
+        return student
+    }
+}
