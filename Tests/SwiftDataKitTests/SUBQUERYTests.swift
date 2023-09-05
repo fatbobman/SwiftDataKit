f34a891998c943687eeba4fb770362ea71a9eaae
fatbobman <xuyang@me.com>
2023-09-05 14:00:18 +0800

    add performance tests

--- Tests/SwiftDataKitTests/SUBQUERYTests.swift
+++ Tests/SwiftDataKitTests/SUBQUERYTests.swift
@@ -11,12 +11,32 @@ import SwiftData
 import XCTest
 
 class SUBQUERYTests: XCTestCase {
-    func testArticle() async throws {
+    func testSubQueryByKit() async throws {
         let container = try ModelContainer(for: ArticleCollection.self, Article.self, Tag.self, configurations: .init(isStoredInMemoryOnly: true))
         let handler = ArticleHandler(modelContainer: container)
-        await handler.dataGenerator()
+        try await handler.dataGenerator()
         let countByKit = await handler.getCollectCountByTagByKit(tagName: "tech")
         let countByQuery = await handler.getCollectCountByTagByQuery(tagName: "tech")
         XCTAssertEqual(countByKit, countByQuery)
+    }
+
+    func testSubQueryByKitPerformance() async throws {
+        let container = try ModelContainer(for: ArticleCollection.self, Article.self, Tag.self, configurations: .init(isStoredInMemoryOnly: true))
+        let handler = ArticleHandler(modelContainer: container)
+        try await handler.dataGenerator(collectionCount: 300, articleCount: 10000)
+        await handler.reset()
+        measureAsync(timeout: 10) {
+            let _ = await handler.getCollectCountByTagByKit(tagName: "tech")
+        }
+    }
+
+    func testSubQueryBySwiftDataPerformance() async throws {
+        let container = try ModelContainer(for: ArticleCollection.self, Article.self, Tag.self, configurations: .init(isStoredInMemoryOnly: true))
+        let handler = ArticleHandler(modelContainer: container)
+        try await handler.dataGenerator(collectionCount: 300, articleCount: 10000)
+        await handler.reset()
+        measureAsync(timeout: 10) {
+            let _ = await handler.getCollectCountByTagByQuery(tagName: "tech")
+        }
     }
 }
